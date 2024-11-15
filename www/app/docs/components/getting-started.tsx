0cd736f2e6eb9d7a7ba81f555fc7a38439b4c7af
Ryan Chou <ryan.chou@gogolook.com>
2024-11-15 17:41:10 +0800

    feat: add getting started page

--- www/app/docs/components/getting-started.tsx
+++ www/app/docs/components/getting-started.tsx
@@ -3,8 +3,13 @@ import Link from 'next/link';
 
 export function GettingStarted() {
   return (
-    <SidebarMenuButton className="pl-4" asChild>
-      <Link href="/docs">Getting Started</Link>
-    </SidebarMenuButton>
+    <>
+      <SidebarMenuButton className="pl-4" asChild>
+        <Link href="/docs">Getting Started</Link>
+      </SidebarMenuButton>
+      <SidebarMenuButton className="border-t pl-4 pt-2" asChild>
+        <Link href="/demo">Demo</Link>
+      </SidebarMenuButton>
+    </>
   );
 }
--- www/app/docs/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Page = () => {
-  return <div>doc home</div>;
-};
-
-export default Page;
